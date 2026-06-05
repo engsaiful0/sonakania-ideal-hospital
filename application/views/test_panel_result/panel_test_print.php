cd1b00e978cbf2eaca64d229f1c139102df554b9
unknown <engsaiful0@gmail.com>
2026-06-05 10:29:09 +0600

    bug fixing

--- application/views/test_panel_result/panel_test_print.php
+++ application/views/test_panel_result/panel_test_print.php
@@ -153,17 +153,19 @@ $compnay = $this->db->where('company_id', '1')->get('company')->row();
                         <tr>
                             <td>Referring Doctor: <b><?php echo html_escape($referring_doctor_label); ?></b></td>
                         </tr>
+                        <tr>
+                            <td>Test Name: <b><?php echo isset($report->panel_name) ? html_escape($report->panel_name) : ''; ?></b></td>
+                        </tr>
                     </table>
                     <?php
 
                     $group_name = $this->db->where('test_group_id', $report->test_group_id)->get('test_group')->row();
                     if ($group_name && isset($group_name->test_group_name) && $group_name->test_group_name) {
                     ?>
-                        <p style="margin-bottom:0;text-align: center;"><strong></strong> <u><?php echo strtoupper(html_escape($group_name->test_group_name . ' Report')); ?></u></p>
+                        <p style="margin-bottom:0;text-align: center;font-weight: bold;"><?php echo strtoupper(html_escape($group_name->test_group_name . ' Report')); ?></p>
 
                     <?php } ?>
                     <p style="text-align: center;font-weight: bold;"><?php echo isset($group_name->machine_name) ? strtoupper(html_escape($group_name->machine_name)) : ''; ?></p>
-                    <p style="text-align: center;font-weight: bold;"><?php echo isset($report->panel_name) ? strtoupper(html_escape($report->panel_name)) : ''; ?></p>
 
                 </div>
                 <?php if (!empty($section_blocks)) { ?>
